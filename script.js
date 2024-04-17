9c26fe50453f2f1bf0738f42d3b57ee9b679e2dd
perropachoncho <ramdavid547@gmail.com>
2024-04-17 12:01:31 -0400

    hell yeah!!

---
+++ script.js
@@ -0,0 +1,77 @@
+console.log(usuario.address)
+;: // string
+const mensaje = 'yep'
+//number
+const digito = 7.888
+//boolean
+const condicion = false
+//array   es una lista de datos que se guardan de forma sucesiva
+const mercado = [
+    "tomates" ,
+    "lechuga",
+    "salsa de maiz",
+    "yogurt",
+    "pan"
+]
+
+// el object es un tipo de dato    que nos permite guardar valores dentro de el pero asignandole una clave especifica a cada valor
+const usario = {
+    email: "ramdavid@gmail.com",
+    password:"90009000",
+    username:"perropachon",
+    address: {
+        city: "Valencia",
+        state: "Carabobo",
+        street: "Madison",
+        code: 404
+    },
+    children: [
+        "juan",
+        "pedro",
+        "levi"
+    ]
+
+    const indefinido = undefined
+
+    const vacio = null
+
+}
+
+const suma =(num1, num2) => {
+   return num1 + num2
+}
+
+const validarUsuario = (usuario) => {
+if (usario.email != null)
+}
+
+console.log(suma(5, 9))
+
+if (1==2) {
+    console.log("se cumple")
+}
+else {
+    console.log("no se cumple")
+}
+
+//Operadores logicos 
+//&&        AND o tambien 'y'
+// //       OR o tambien 'o'
+//=         Asignacion
+//
+//==        comparacion igual
+//!=         desigualdad
+//>=        mayor o igual
+//>         mayor
+//<=        menor o igual
+//<         menor
+//Operadores Matematicos
+//+        suma o adicion
+//-        resta o sustraccion
+//*        multiplicacion
+//**       poteciacion
+// /       division 
+// %       ¿
+// una funcion es un bloque de codigo que contiene una serie de instrucciones o sentencias
+// una constante o variable es un espacio en memoria para guardar
+// son como variables que sirven dentro de la funcion
